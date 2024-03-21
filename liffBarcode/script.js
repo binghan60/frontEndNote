9d0729aede361fa6e7fd27e41ee7c8e38ce510ad
binghan60 <binghan60@gmail.com>
2024-03-21 13:51:08 +0800

    quagga

---
+++ liffBarcode/script.js
@@ -0,0 +1,48 @@
+﻿const liffID = '2001461566-Ywj7B6mg',
+	LiffAppId = liffID;
+
+document.addEventListener('DOMContentLoaded', () => {
+	init(liffID);
+});
+
+async function init() {
+	await liff.init({ liffId: liffID });
+	if (!liff.isLoggedIn()) {
+		liff.login({ redirectUri: location.href });
+	}
+	UserLineID = (await liff.getProfile()).userId;
+	const buttons = document.querySelectorAll('.button-85');
+	buttons.forEach(function (button, index) {
+		button.addEventListener('click', async function () {
+			btnIndex = index;
+			Quagga.init(
+				{
+					inputStream: {
+						name: 'Live',
+						type: 'LiveStream',
+						target: document.querySelector('#barcodeScanner'),
+					},
+					decoder: {
+						readers: ['code_128_reader'],
+					},
+				},
+				function (err) {
+					if (err) {
+						console.error('Quagga 初始化失败', err);
+						return;
+					}
+					console.log('Quagga 初始化成功，开始扫描');
+					Quagga.start();
+				}
+			);
+
+			Quagga.onDetected(function (data) {
+				const scannedCode = data.codeResult.code;
+				document.querySelectorAll('input[type=text]')[btnIndex].value = scannedCode;
+				Quagga.stop();
+				document.getElementById('barcodeScanner').innerHTML = '';
+				document.getElementById('scanResult').innerHTML = '';
+			});
+		});
+	});
+}
